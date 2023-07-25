ae2f2d8bf90e7cfc215397cf612bc5a8fea9c28e
Horleng <ly.horleng.1221@rupp.edu.kh>
2023-07-25 16:58:46 +0700

    error fix

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ cloudinary.config({
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-  origin: ["https://kh-chat.vercel.app/","https://kh-chat-git-main-horlengly.vercel.app/","https://kh-chat-9dv4chxk7-horlengly.vercel.app/"],
+  origin: ["https://kh-chat.vercel.app/"],
   methods: ["GET", "POST", "DELETE", "PATCH"]
 }));
 // routes
